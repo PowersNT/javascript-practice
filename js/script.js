0d3b9ca7028661c39e20d7e42f3f5ab509d2100e
Patricia E. Powers <patricia.e.powers@gmail.com>
2022-08-10 20:43:50 -0400

    created take-home branch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("Hello, everyone");
+
+
+/* This is a multi-line comment
+with two lines */
+
+console.log("Tricia Powers");
+
+// This is a single-line comment
+
+console.log("avocado toast");
